66f007c9d5f098615639b11f08c97f67d17f54be
wwllaad <wwllaad@mail.ru>
2018-01-24 16:49:46 +0300

    FE project structure was created.

---
+++ src/main/resources/static/client/app/MeteoApp.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MeteoApp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>TEST</div>
+        )
+    }
+}
+export default MeteoApp ;
---
+++ src/main/resources/static/client/app/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import MeteoApp from './MeteoApp.jsx';
+
+render(<MeteoApp />, document.getElementById('root'));
