70d1ca130f92700a53df0dc54062985340606bf2
wwllaad <wwllaad@mail.ru>
2018-01-24 16:05:48 +0300

    Initial commit

---
+++ src/main/java/com/sevgmo/stationpassport/StationPassportApplication.java
@@ -0,0 +1,12 @@
+package com.sevgmo.stationpassport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationPassportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationPassportApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sevgmo/stationpassport/StationPassportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sevgmo.stationpassport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StationPassportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
