1bd86110e11bb7e60e4413bc924d18f2dda437d9
wwllaad <wwllaad@mail.ru>
2018-03-12 13:35:18 +0300

    Selenium WebDriver login-test added.

--- src/test/java/com/sevgmo/stationpassport/login/LoginTest.java
+++ src/test/java/com/sevgmo/stationpassport/login/LoginTest.java
@@ -1,49 +1,39 @@
 package com.sevgmo.stationpassport.login;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
+    @Test
+    public void userLogin() {
+        File file = new File("C:/chromedriver_win32/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
-    private static WebDriver driver;
-
-    @BeforeClass
-    public static void setup() {
-
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://localhost:8080/login");
-    }
-    @Test
-    public void userLogin() {
+
         WebElement loginField = driver.findElement(By.name("username"));
         loginField.sendKeys("Vlad");
         WebElement passwordField = driver.findElement(By.name("password"));
         passwordField.sendKeys("root");
         WebElement loginButton = driver.findElement(By.name("submit"));
         loginButton.click();
-        WebElement testPage = driver.findElement(By.id("root"));
-        String testMessage = testPage.getText();
-        Assert.assertEquals("TEST", testMessage);
-    }
-    @AfterClass
-    public static void tearDown() {
-//        WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
-//        menuUser.click();
-//        WebElement logoutButton = driver.findElement(By.id("login__logout"));
-//        logoutButton.click();
+        WebElement label = driver.findElement(By.className("navbar-header"));
+        String testMessage = label.getText();
+        Assert.assertEquals("Метеостанции Крыма", testMessage);
         driver.quit();
     }
 }
+
+
+
